58c7b3bdec950345b91e51ad440d29443e48b8c4
School <school@Matthews-MacBook-Pro.local>
2018-11-13 12:05:46 -0500

    First commit of bootcamp

---
+++ python101.py
@@ -0,0 +1,38 @@
+print "Hello, World"
+print("Hello, World")
+
+print """ 
+    It was a dark and clear night.
+    Nothing interesting happened.
+"""
+
+print 'single ticks and double quotes both print the same'
+print "Patrice O'Neal"
+print 'Patrice O\'Neal\n'
+
+# \n creates a new line... see above
+# Variables
+# - Anything that can be typed with the keyboard.
+# variables do not make the program faster.
+# They make the program slower!
+# Variables make it easier for us to write programs.
+
+theBestClass = "the 11-18 immersive"
+print theBestClass
+
+
+
+# Data Types
+# - Programming languages see different types of variables
+# differently
+# - String - it's just a bunch of words between quotes for English users
+# - Number - Something with numbers
+# Floats, Integers
+# -- float = it has a . in it
+# -- integer doesn't have a .
+# - Booleans - true or falso, on or off, 1 or 0, yes or no, right or left
+# - list - list of things. a single variable with a bunch of parts
+# - Dictionaries - variable of variables
+# - Objects - super dictionaries
+
+# Primitive Data types - string, number, boolean
