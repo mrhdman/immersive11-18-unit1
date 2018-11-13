fc81882008f9165da69ae2c9e0d293960e3ee0f7
School <school@Matthews-MacBook-Pro.local>
2018-11-13 14:29:21 -0500

    Add operators

--- python101.py
+++ python101.py
@@ -1,50 +1,80 @@
-print "Hello, World"
-print("Hello, World")
+# print "Hello, World"
+# print("Hello, World")
 
-print """ 
-    It was a dark and clear night.
-    Nothing interesting happened.
-"""
+# print """ 
+#     It was a dark and clear night.
+#     Nothing interesting happened.
+# """
 
-print 'single ticks and double quotes both print the same'
-print "Patrice O'Neal"
-print 'Patrice O\'Neal\n'
+# print 'single ticks and double quotes both print the same'
+# print "Patrice O'Neal"
+# print 'Patrice O\'Neal\n'
 
-# \n creates a new line... see above
-# Variables
-# - Anything that can be typed with the keyboard.
-# variables do not make the program faster.
-# They make the program slower!
-# Variables make it easier for us to write programs.
+# # \n creates a new line... see above
+# # Variables
+# # - Anything that can be typed with the keyboard.
+# # variables do not make the program faster.
+# # They make the program slower!
+# # Variables make it easier for us to write programs.
 
-theBestClass = "the 11-18 immersive"
-print theBestClass
+# theBestClass = "the 11-18 immersive"
+# print theBestClass
 
 
 
-# Data Types
-# - Programming languages see different types of variables
-# differently
-# - String - it's just a bunch of words between quotes for English users
-# - Number - Something with numbers
-# Floats, Integers
-# -- float = it has a . in it
-# -- integer doesn't have a .
-# - Booleans - true or falso, on or off, 1 or 0, yes or no, right or left
-# - list - list of things. a single variable with a bunch of parts
-# - Dictionaries - variable of variables
-# - Objects - super dictionaries
+# # Data Types
+# # - Programming languages see different types of variables
+# # differently
+# # - String - it's just a bunch of words between quotes for English users
+# # - Number - Something with numbers
+# # Floats, Integers
+# # -- float = it has a . in it
+# # -- integer doesn't have a .
+# # - Booleans - true or falso, on or off, 1 or 0, yes or no, right or left
+# # - list - list of things. a single variable with a bunch of parts
+# # - Dictionaries - variable of variables
+# # - Objects - super dictionaries
 
-# Primitive Data types - string, number, boolean
-month = "November";
-print type(month)
-date = 13
-print type (date)
-dateAsFloat = 13.0
-print type(dateAsFloat)
-aBool = True 
-print type(aBool)
-aList = []
-print type(aList)
-aDictionary = {}
-print type(aDictionary)
+# # Primitive Data types - string, number, boolean
+# month = "November";
+# print type(month)
+# date = 13
+# print type (date)
+# dateAsFloat = 13.0
+# print type(dateAsFloat)
+# aBool = True 
+# print type(aBool)
+# aList = []
+# print type(aList)
+# aDictionary = {}
+# print type(aDictionary)
+
+# # concatenate is programming speak for add things together
+# first = "Matt"
+# last = "Havins"
+# # fullName = first + last
+# fullName = first + " " + last
+# print fullName
+
+# fourteen = 10 + 4
+# print fourteen
+# fourteen = "10" + "4"
+# print fourteen 
+
+# # cast = change a variable to a new data type
+# fourteen = int("10") + 4
+
+
+
+
+# Math = +, -, /, *, %
+print 2+2
+print 2-2
+print 2/2
+print 2*2
+# % = modulus. Modulus divides the number and gives you the remainder
+print 2%2 
+
+print 2**3
+print 10**8
+print "lol "*7
